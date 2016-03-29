424503f53b4301f6275424faa0078542417da9dc
Brian Jacobel <bjacobel@gmail.com>
2016-03-28 23:10:56 -0400

    Switch to postcss/autoprefixer

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,13 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
-const bourbon = require('node-bourbon');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
+const postcssImport = require('postcss-import');
+const stylelint = require('stylelint');
 
 const isProd = process.env.NODE_ENV === 'production';
-
-const sassPaths = bourbon.includePaths.map((sassPath) => {
-  return 'includePaths[]=' + sassPath;
-}).join('&');
 
 const wpconfig = {
   entry: {
@@ -29,33 +27,31 @@ const wpconfig = {
         loader: 'babel'
       },
       {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style', 'css!sass?' + sassPaths)
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader'
       },
       {
         test: /\.(eot|ttf|woff|svg)(\?[a-z0-9=]+)?$/,
         loader: 'file-loader'
-      },
-      {
-        test: /packery/,
-        loader: 'imports?define=>false&this=>window'
       }
     ],
     noParse: [
       /aws\-sdk/
     ]
   },
-  sassLoaderConfig: {
-    sourceComments: !isProd,
-    outputStyle: 'expanded',
-    sourceMap: false
+  postcss() {
+    return [
+      stylelint,
+      autoprefixer({ browsers: ['last 2 versions'] }),
+      precss,
+      postcssImport
+    ];
   },
   resolve: {
-    extensions: ['', '.js', '.json', '.jsx', '.scss']
+    extensions: ['', '.js', '.json', '.jsx', '.css']
   },
   plugins: [
     new webpack.NoErrorsPlugin(),
-    new ExtractTextPlugin(`[name].css`),
     new webpack.DefinePlugin({
       'process.env': {
         'production': isProd
