008422fc7b30abe656bf7d5162c34611a0319d48
Brian Jacobel <bjacobel@gmail.com>
2016-08-21 23:50:19 -0500

    Spinners while gif is loading

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,15 +1,50 @@
 import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
 
 export default class Gif extends Component {
   componentWillMount() {
     const { watchForSize, id, name, image } = this.props;
 
+    this.state = {
+      loaded: false,
+      loading: false
+    };
+
+    this.onImageLoad = () => {
+      this.setState({
+        loaded: true,
+        loading: false
+      });
+      image.removeEventListener('load', this.onImageLoad);
+    };
+    this.startLoad = () => {
+      this.setState({ loading: true });
+    };
+
     watchForSize(image, id, name, document.querySelector('.column.gifs').scrollWidth);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.image.src.startsWith('https://gifs.')) {
+      if (nextProps.image.complete) {
+        this.onImageLoad();
+      } else {
+        nextProps.image.addEventListener('load', this.onImageLoad);
+      }
+    }
   }
 
   render() {
     const { image } = this.props;
-    return <img src={ image.src } role="presentation" />;
+    const { loaded, loading } = this.state;
+    return (
+      <div>
+        <div className={ classnames('loading-indicator', { loaded, loading }) } >
+          <div className="spinner" />
+        </div>
+        <img src={ image.src } role="presentation" onMouseOver={ this.startLoad } />
+      </div>
+    );
   }
 }
 
