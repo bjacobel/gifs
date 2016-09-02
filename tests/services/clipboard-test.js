bde41e709ffad25452388f0b1a51e428f21b2bce
Brian Jacobel <bjacobel@gmail.com>
2016-09-02 13:03:02 -0400

    Added test file for clipboard service, but I'm a little unsure how to test it

---
+++ tests/services/clipboard-test.js
@@ -0,0 +1,19 @@
+jest.unmock('../../src/services/clipboard');
+
+import { copy } from '../../src/services/clipboard';
+
+describe('clipboard service', () => {
+  describe('clipboard.copy', () => {
+    it('copies passed text to the clipboard', () => {
+
+    });
+
+    it('calls the handler only once', () => {
+
+    });
+
+    it("removes the listener after it's done", () => {
+
+    });
+  });
+});
