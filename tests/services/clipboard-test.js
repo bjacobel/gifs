26a1af44acafebfb6068baea6c73bbec6f354ae8
Brian Jacobel <bjacobel@gmail.com>
2016-09-04 22:18:38 -0400

    fix build

--- tests/services/clipboard-test.js
+++ tests/services/clipboard-test.js
@@ -4,16 +4,18 @@ import { copy } from '../../src/services/clipboard';
 
 describe('clipboard service', () => {
   describe('clipboard.copy', () => {
-    it('copies passed text to the clipboard', () => {
+    document.execCommand = jest.fn();
 
+    it('copies passed text to the clipboard', () => {
+      copy();
     });
 
     it('calls the handler only once', () => {
-
+      copy();
     });
 
     it("removes the listener after it's done", () => {
-
+      copy();
     });
   });
 });
