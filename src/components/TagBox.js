eb9caeb7d927dae439a1f1d1667d75e01e7fc6c1
Brian Jacobel <bjacobel@gmail.com>
2016-11-26 21:02:07 -0500

    Shouldn't have removed hasOwnProperty

--- src/components/TagBox.js
+++ src/components/TagBox.js
@@ -42,7 +42,7 @@ class TagBox extends Component {
     } else {
       let tagList = null;
 
-      if (tags.activeGifId) {
+      if (tags[activeGifId]) {
         tagList = tags[activeGifId].map((tag) => {
           return (
             <Tag key={ tag.id } id={ tag.id } content={ tag.text } />
