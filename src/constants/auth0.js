06ec9b29191e97e1055fcfee0627ab5026335882
Brian Jacobel <bjacobel@gmail.com>
2016-12-24 18:46:42 -0600

    Been doing prod-only variables wrong for a long time

--- src/constants/auth0.js
+++ src/constants/auth0.js
@@ -1,3 +1,4 @@
 export const CLIENT_ID = 'HoqDLSJ6Aq6P7vrsJ1iDzn6eKOADa6Tg';
 export const DOMAIN = 'bjacobel.auth0.com';
-export const REDIRECT_URL = process.env.production ? 'https://gifs.bjacobel.com' : 'http://localhost:8080';
+export const REDIRECT_URL = process.env.NODE_ENV === 'production' ?
+  'https://gifs.bjacobel.com' : 'http://localhost:8080';
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const SHOW_DEV_TOOLS = !process.env.production;
+export const SHOW_DEV_TOOLS = process.env.NODE_ENV !== 'production';
 export const ROOT_DOMAIN = 'bjacobel.com';
 export const ROOT_URL = `https://gifs.${ROOT_DOMAIN}/`;
 export const THUMB_URL = `https://gifthumbs.${ROOT_DOMAIN}/`;
