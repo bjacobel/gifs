afe7f88294aad435322abd01af5d30328d680dd1
Brian Jacobel <bjacobel@gmail.com>
2016-12-24 18:35:43 -0600

    Replace a bunch of hacky custom infinite scrolling code with react-infinite

--- src/actions/gifs.js
+++ src/actions/gifs.js
@@ -1,13 +1,11 @@
 import { obtainCurrentRole } from '../services/cognito';
 import { getBucketContents } from '../services/s3';
-import { isGifVisible } from '../services/infinite';
 import { updateSearchIndexAsync } from './search';
 
 export const GET_GIFS_REQUESTED = 'GET_GIFS_REQUESTED';
 export const GET_GIFS_FAILED = 'GET_GIFS_FAILED';
 export const GET_GIFS_SUCCEEDED = 'GET_GIFS_SUCCEEDED';
 export const GET_IMAGE_SIZE_SUCCEEDED = 'GET_IMAGE_SIZE_SUCCEEDED';
-export const FOUND_VISIBLE_GIFS = 'FOUND_VISIBLE_GIFS';
 
 function getGifsRequested() {
   return { type: GET_GIFS_REQUESTED };
@@ -55,29 +53,5 @@ export function watchForSize(img, id, src, containerWidth) {
         dispatch(getSizeSucceeded(img.height, img.width, observedHeight, containerWidth, id, src));
       }
     }, 10);
-  };
-}
-
-function foundVisibleGifs(visibleLowRange, visibleHighRange) {
-  return { type: FOUND_VISIBLE_GIFS, payload: { visibleLowRange, visibleHighRange } };
-}
-
-export function updateVisibleGifs(gifs) {
-  return (dispatch) => {
-    if (!gifs) {
-      dispatch(foundVisibleGifs(0, 10));
-    } else {
-      const visibles = [];
-
-      for (let i = 0; i < gifs.length; i += 1) {
-        if (isGifVisible(gifs, i)) {
-          visibles.push(i);
-        } else if (visibles.length > 0) {
-          break;
-        }
-      }
-
-      dispatch(foundVisibleGifs(Math.max(visibles[0] - 5, 0), visibles[visibles.length - 1] + 5));
-    }
   };
 }
