74bc8b918a93feb3e5d27da19b802baea33ea94c
Brian Jacobel <bjacobel@gmail.com>
2016-12-29 15:55:10 -0600

    Jest snapshot tests for gif component

--- tests/components/Gif-test.js
+++ tests/components/Gif-test.js
@@ -1,25 +1,62 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
 import Gif from '../../src/components/Gif';
-import fakeImage from './image.gif';
 
 describe('Gif component', () => {
   let gif;
-  let watchForSize = jest.fn();
+  let image;
 
   beforeEach(() => {
-      gif = renderer.create(
-        <Gif
-          image={ { src: fakeImage } }
-          id={ 1 }
-          name="testgif"
-          watchForSize={ watchForSize }
-        />
-      );
+    document.querySelector = jest.fn(() => ({ scrollWidth: 500 }));
+    image = new Image();
+    image.src = 'https://gifthumbs.bjacobel.com/partyparrot.gif';
+
+    gif = (
+      <Gif
+        image={ image }
+        id="asdf"
+        name="testgif"
+        watchForSize={ jest.fn() }
+      />
+    );
   });
 
   it('matches the initial state snapshot', () => {
-
+    expect(shallow(gif)).toMatchSnapshot();
   });
-})
+
+  it('matches snapshot after load of the full gif is triggered', () => {
+    const renderedGif = shallow(gif);
+
+    renderedGif.find('img').simulate('mouseOver');
+    image.src = 'https://gifs.bjacobel.com/partyparrot.gif';
+    renderedGif.setProps('image', image);
+
+    expect(renderedGif).toMatchSnapshot();
+  });
+
+  it('matches snapshot after load of the full gif is finished', () => {
+    const renderedGif = shallow(gif);
+
+    renderedGif.find('img').simulate('mouseOver');
+    image.src = 'https://gifs.bjacobel.com/partyparrot.gif';
+    renderedGif.setProps('image', image);
+    image.dispatchEvent(new Event('load'));
+    renderedGif.update();
+
+    expect(renderedGif).toMatchSnapshot();
+  });
+
+  it('unmounts and removes the event listener', () => {
+    const renderedGif = shallow(gif);
+
+    renderedGif.find('img').simulate('mouseOver');
+    image.src = 'https://gifs.bjacobel.com/partyparrot.gif';
+    renderedGif.setProps('image', image);
+    image.dispatchEvent(new Event('load'));
+    renderedGif.unmount();
+
+    expect(image.src).toEqual('//:0');
+  });
+});
