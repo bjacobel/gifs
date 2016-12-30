fef994d21ed330d613fd97421b57941b3831c890
Brian Jacobel <bjacobel@gmail.com>
2016-12-29 18:56:14 -0600

    Fix and tests for search reducer

--- tests/reducers/search-test.js
+++ tests/reducers/search-test.js
@@ -1,11 +1,63 @@
+import {
+  UPDATE_INDEX_SUCCEEDED,
+  SEARCH_SUCCEEDED,
+} from '../../src/actions/search';
+
+import {
+  searchIndex,
+  searchResults,
+} from '../../src/reducers/search';
+
 describe('search reducers', () => {
   describe('searchIndex reducer', () => {
-    it('', () => {
+    it('updates the saved search index', () => {
+      const newIndex = {
+        thisIs: 'the new',
+        search: 'index',
+      };
+
+      expect(
+        searchIndex(
+          { foo: 'bar' },
+          {
+            type: UPDATE_INDEX_SUCCEEDED,
+            payload: {
+              newIndex,
+            },
+          },
+        ),
+      ).toEqual(newIndex);
+    });
+
+    it('does nothing by default', () => {
+      expect(searchIndex({}, { type: 'none' })).toEqual({});
     });
   });
 
   describe('searchResults reducer', () => {
-    it('', () => {
+    it('adds a list of search results to state', () => {
+      const ids = [
+        'these',
+        'are',
+        'gif',
+        'ids',
+      ];
+
+      expect(
+        searchResults(
+          { foo: 'bar' },
+          {
+            type: SEARCH_SUCCEEDED,
+            payload: {
+              searchResults: ids,
+            },
+          },
+        ),
+      ).toEqual(ids);
+    });
+
+    it('does nothing by default', () => {
+      expect(searchResults([], { type: 'none' })).toEqual([]);
     });
   });
 });
